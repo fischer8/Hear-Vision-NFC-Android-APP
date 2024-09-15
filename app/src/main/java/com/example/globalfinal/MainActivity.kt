585dc7231a29ca2d120b15f2c1786477354ea7a1
fischer8 <fischxr@hotmail.com>
2024-09-15 08:21:11 -0300

    first commit

---
+++ app/src/main/java/com/example/globalfinal/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.globalfinal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import android.os.Parcelable
+import android.content.Context
+import android.provider.Settings
+
+import android.app.Activity
+import android.nfc.tech.MifareUltralight
+import java.io.IOException
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
+import android.nfc.tech.NfcA
+import android.util.Log
+import android.widget.Toast
+
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+    private var nfcAdapter: NfcAdapter? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate Called")
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        Log.d(TAG, "${nfcAdapter}")
+
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "2NFC CREATE INSTANCE")
+        val intent = Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+        val techList = arrayOf(arrayOf(NfcAdapter::class.java.name, MifareUltralight::class.java.name))
+        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, null, techList)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d(TAG, "10000NFC CREATE INSTANCE")
+
+        Log.d(TAG, "ENTRO NO IF")
+        //val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        val tag: Tag? = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG, Tag::class.java)
+        Log.d(TAG, "tag --------- ${tag}")
+        tag?.let {
+            val mifare = MifareUltralight.get(it)
+            readFromTag(mifare) // Chamando o método readFromTag corretamente
+        }
+
+    }
+
+    private fun readFromTag(mifare: MifareUltralight) {
+        Log.d(TAG, "TENTANDO LER SA DISSGRASSA")
+        val numPages = 16
+        val stringBuilder = StringBuilder()
+        try {
+            mifare?.connect()
+
+            for (i in 6 until numPages step 4) {
+                val payload = mifare.readPages(i)
+                if (payload != null) {
+                    val pageData = String(payload, Charsets.UTF_8)
+                    val cleanPageData = cleanText(pageData)
+                    stringBuilder.append(cleanPageData)
+                } else {
+                    Log.d(TAG, "Page $i is null")
+                }
+            }
+
+            val concatenatedData = stringBuilder.toString().trim().drop(1)
+            Log.d(TAG, "Concatenated Data: $concatenatedData")
+
+            Toast.makeText(this, "Tag Data: $concatenatedData", Toast.LENGTH_SHORT).show()
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            try {
+                mifare?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun isPrintableCharacter(c: Char): Boolean {
+        return c.isLetterOrDigit() || c.isWhitespace() || c.isISOControl()
+    }
+
+    private fun cleanText(data: String): String {
+        return data.filter { isPrintableCharacter(it) }
+    }
+
+
+}
