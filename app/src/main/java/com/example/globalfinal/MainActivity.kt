ad841f7394c023ae5cc42c4a8b9d736c67d0d353
fischer8 <fischxr@hotmail.com>
2024-09-17 00:15:47 -0300

    lateste

--- app/src/main/java/com/example/globalfinal/MainActivity.kt
+++ app/src/main/java/com/example/globalfinal/MainActivity.kt
@@ -24,13 +24,14 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var nfcAdapter: NfcAdapter? = null
     private lateinit var tts: TextToSpeech
-    private var rightBtnPress = false
-    private var leftBtnPress = false
     private var currentSpeechRate: Float = 1.0f
+    private var isPressed: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        currentSpeechRate = getSpeechRate()
 
         tts = TextToSpeech(this, this)
 
@@ -39,69 +40,70 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         val rightButton = findViewById<Button>(R.id.rightBtn)
         val leftButton = findViewById<Button>(R.id.leftBtn)
 
+        setupButtonHold(rightButton) { increase() }  // Pressione por 1 segundo para aumentar
+        setupButtonHold(leftButton) { decrease() }
+    }
 
-        rightButton.setOnTouchListener { _, event ->
+    override fun onStart() {
+        super.onStart()
+        tts.setSpeechRate(currentSpeechRate)
+    }
+
+    private fun setupButtonHold(button: Button, action: () -> Unit) {
+        button.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     // Inicia o temporizador quando o botão for pressionado
-                    rightBtnPress = true
-                    rightButton.postDelayed({
-                        if (rightBtnPress) {
-                            increase() // Função chamada após 2 segundos de pressão
+                    isPressed = true
+                    button.postDelayed({
+                        if (isPressed) {
+                            action() // Chama a ação após o tempo de atraso
                         }
-                    }, 2000) // 2000 ms = 2 segundos
+                    }, 1000)
                     true
                 }
                 MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    // Cancela o temporizador se o botão for solto antes de 2 segundos
-                    rightBtnPress = false
+                    // Cancela o temporizador se o botão for solto antes do tempo de atraso
+                    isPressed = false
                     true
                 }
                 else -> false
             }
         }
-
-        leftButton.setOnTouchListener { _, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-
-                    leftBtnPress = true
-                    leftButton.postDelayed({
-                        if (leftBtnPress) {
-                            decrease() // Função chamada após 2 segundos de pressão
-                        }
-                    }, 2000) // 2000 ms = 2 segundos
-                    true
-                }
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    // Cancela o temporizador se o botão for solto antes de 2 segundos
-                    leftBtnPress = false
-                    true
-                }
-                else -> false
-            }
-        }
-
     }
+
+    private fun saveSpeechRate(value: Float) {
+        val sharedPref = getSharedPreferences("speechRate", MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putFloat("speechRate", value)
+        editor.apply() // `apply()` salva de forma assíncrona; `commit()` salva de forma síncrona.
+    }
+
+    private fun getSpeechRate(): Float {
+        val sharedPref = getSharedPreferences("speechRate", MODE_PRIVATE)
+        return sharedPref.getFloat("speechRate", 1.0f) // Retorna o valor salvo ou 0.0f se não existir.
+    }
+
 
     private fun increase() {
         currentSpeechRate += 0.5f
-
+        saveSpeechRate(currentSpeechRate)
         tts.setSpeechRate(currentSpeechRate)
         speak("Velocidade aumentada")
-        Toast.makeText(this, "$currentSpeechRate", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "$currentSpeechRate", Toast.LENGTH_SHORT).show()
     }
 
     private fun decrease() {
         currentSpeechRate -= 0.5f
-
+        saveSpeechRate(currentSpeechRate)
         tts.setSpeechRate(currentSpeechRate)
         speak("Velocidade reduzida")
-        Toast.makeText(this, "$currentSpeechRate", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "$currentSpeechRate", Toast.LENGTH_SHORT).show()
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
+
 
             val result = tts.setLanguage(Locale("pt", "BR"))
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
@@ -175,7 +177,7 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
             }
             val concatenatedData = stringBuilder.toString().trim().drop(1)
             Log.d(TAG, "Concatenated Data: $concatenatedData")
-            Toast.makeText(this, "Tag Data: $concatenatedData", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Tag Data: $concatenatedData", Toast.LENGTH_SHORT).show()
 
 
             speak(concatenatedData)
